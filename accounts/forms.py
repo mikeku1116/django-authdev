d41dd8223c39ddc17b131e0678a07f121b8da82e
mikeku1116 <mikekuforgit@gmail.com>
2020-04-02 20:44:03 +0800

    修改LoginForm登入表單

--- accounts/forms.py
+++ accounts/forms.py
@@ -33,9 +33,11 @@ class RegisterForm(UserCreationForm):
 class LoginForm(forms.Form):
 
     username = forms.CharField(
+        label="帳號",
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
     password = forms.CharField(
+        label="密碼",
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
