179b4de668c3b77149043da1ac27d88d74caa1c2
mikeku1116 <mikekuforgit@gmail.com>
2020-04-02 20:41:02 +0800

    修改RegisterForm註冊表單

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,19 +6,23 @@ from django.contrib.auth.models import User
 class RegisterForm(UserCreationForm):
 
     username = forms.CharField(
+        label="帳號",
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
     email = forms.EmailField(
+        label="電子郵件",
         widget=forms.EmailInput(attrs={'class': 'form-control'})
     )
 
     password1 = forms.CharField(
+        label="密碼",
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
 
     password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        label="密碼確認",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
 
     class Meta:
