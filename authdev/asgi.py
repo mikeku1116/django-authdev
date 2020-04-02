44cfbb1648ea937a9dabf6a47dd2f19f1bda249f
mikeku1116 <mikekuforgit@gmail.com>
2020-04-02 12:11:42 +0800

    專案初始化

---
+++ authdev/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for authdev project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authdev.settings')
+
+application = get_asgi_application()
