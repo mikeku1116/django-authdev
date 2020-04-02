4647a9231f186eacc2d8cf938757de01aed495d3
mikeku1116 <mikekuforgit@gmail.com>
2020-04-02 19:11:40 +0800

    首頁檢視函式加上login_required裝飾詞

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import RegisterForm, LoginForm
 
-
+# 首頁
+@login_required(login_url="Login")
 def index(request):
     return render(request, 'accounts/index.html')
 
