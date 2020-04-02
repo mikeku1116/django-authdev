bc62217f4abfd708818d26473550aef3ab794421
mikeku1116 <mikekuforgit@gmail.com>
2020-04-02 15:01:13 +0800

    完成將註冊資料寫入資料庫中

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,11 @@ def register(request):
 
     form = UserCreationForm()
 
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     context = {
         'form': form
     }
